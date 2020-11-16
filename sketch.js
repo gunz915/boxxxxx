9a2a69d718d284b6201f9d75614062201d65b12c
gunz915 <73839392+gunz915@users.noreply.github.com>
2020-11-16 17:44:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+var engine,world
+var box1
+var box2
+function setup(){
+  var canvas = createCanvas(400,400)
+  engine=Engine.create();
+  world=engine.world
+ 
+box1=new Box(200,100,50,50);
+box2=new Box(100,50,50,100)
+}
+function draw(){
+  background("purple")
+  Engine.update(engine);
+  box1.display();
+  box2.display();
+}
